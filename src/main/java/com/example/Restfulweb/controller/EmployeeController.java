0270a0cc8c513b95034e9dfbf1bbd8387bb87194
abdul <abdul.tanveer@gmail.com>
2024-02-27 15:24:33 +0530

    getmapping

--- src/main/java/com/example/Restfulweb/controller/EmployeeController.java
+++ src/main/java/com/example/Restfulweb/controller/EmployeeController.java
@@ -11,4 +11,14 @@ public class EmployeeController {
     String sayHello(){
         return "hello and welcome";
     }
+
+    @GetMapping("/add")
+    String add(){
+        return "the sum is";
+    }
+
+    @GetMapping("/weather")
+    String getWeather(){
+        return  "the  weather is good";
+    }
 }
